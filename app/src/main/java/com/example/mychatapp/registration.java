20bcfe22d23ee1efc03267f47dfef10d372860cd
julfikar-23 <julfikar.sust.swe.21@gmail.com>
2025-02-28 13:04:16 +0600

    Secon Commit

--- app/src/main/java/com/example/mychatapp/registration.java
+++ app/src/main/java/com/example/mychatapp/registration.java
@@ -1,6 +1,5 @@
 package com.example.mychatapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,26 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class MainActivity extends AppCompatActivity {
-
-
-    FirebaseAuth auth;
+public class registration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        auth = FirebaseAuth.getInstance();
-        if(auth.getCurrentUser()==null){
-            Intent intent = new Intent(MainActivity.this, login.class);
-            startActivity(intent);
-        }
-
-
+        setContentView(R.layout.activity_registration);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
