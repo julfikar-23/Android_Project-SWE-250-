eba22ee649a855d951ace72ad41b60e59b7f0832
julfikar-23 <julfikar.sust.swe.21@gmail.com>
2025-03-12 10:46:08 +0600

    Chat Screen

--- app/src/main/java/com/example/mychatapp/chatwindo.java
+++ app/src/main/java/com/example/mychatapp/chatwindo.java
@@ -1,4 +1,33 @@
 package com.example.mychatapp;
 
-public class chatwindo {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class chatwindo extends AppCompatActivity {
+    String reciverimg, reciverUid,reciverName,SenderUID;
+    CircleImageView profile;
+    TextView reciverNName;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatwindo);
+        getActionBar().hide();
+
+        reciverName = getIntent().getStringExtra("nameee");
+        reciverimg = getIntent().getStringExtra("reciverImg");
+        reciverUid = getIntent().getStringExtra("uid");
+
+        profile = findViewById(R.id.profileimgg);
+        reciverNName = findViewById(R.id.recivername);
+        Picasso.get().load(reciverimg).into(profile);
+        reciverNName.setText(""+reciverName);
+    }
 }
